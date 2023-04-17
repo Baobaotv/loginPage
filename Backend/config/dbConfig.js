4d00fb638cf450cf46dda873ee1aab33efa701a8
Baobaotv <nguyenbaowwe@gmail.com>
2023-04-17 23:30:41 +0700

    update database

--- Backend/config/dbConfig.js
+++ Backend/config/dbConfig.js
@@ -2,10 +2,10 @@ const mysql=require('mysql');
 const env=require('dotenv').config()
 
 const dbConfig=mysql.createConnection({
-    host:'localhost',
-    database:'userdb',
-    user:process.env.User,
-    password:process.env.PASSWORD
+    host:'us-cdbr-east-05.cleardb.net',
+    database:'heroku_8b2497654a2aa12',
+    user:'b8ed1f151830c1',
+    password:'3afc372c'
 })
 dbConfig.connect((error)=>{
     if(error) throw error
